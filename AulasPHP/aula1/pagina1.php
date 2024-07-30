2712f692a007dae9ac9cc122aff3fa84d2096659
Wilson Yuri <wilsonyuri@icloud.com>
2024-07-30 08:02:52 -0300

    Changed folders

---
+++ AulasPHP/aula1/pagina1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+    echo "Hello World! <br>";
+    
+    // phpinfo();
+    // $idade = 18;
+    // echo "Minha idade é $idade";
+    // var_dump($idade);
+    /*
+    $altura = 1.83;
+    echo "Minha altura é " . $altura . "<br>";
+    var_dump($altura);
+    */
+    $nome = "Wilson";
+    echo "Meu nome é $nome";
+    var_dump($nome);
+  
+
+  ?>
+</body>
+</html>
